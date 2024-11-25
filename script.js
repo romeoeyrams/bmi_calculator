763e62c211e9027ebe5496b9ecad390f361a7880
Romeo Gborgblorvor <colbby@gmail.com>
2024-11-25 11:22:06 -0400

    script completed for bmi calculator

--- script.js
+++ script.js
@@ -1,84 +1,58 @@
-// function calculateBMI() {
-//     //Retrieve input values
-//     const gender = document.getElementById('gender').value;
-//     const age = parseInt(document.getElementById('age').value);
-//     const height = parseFloat(document.getElementById('height').value);
-//     const weight = parseFloat(document.getElementById('weight').value);
-    
-//     //Validate input
-//     if (isNaN(age) || age <= 0 || age > 120) {
-//         alert('Please enter a valid age.');
-//         return;
-//     }
+const form = document.querySelector("form");
+const result = document.querySelector("#result");
 
-//     if (isNaN(height) || height <= 0 || height > 3) {
-//         alert('Please enter a valid height.');
-//         return;
-//     }
 
-//     if (isNaN(weight) || weight <= 0 || weight > 500){
-//         alert('Please enter a valid weight.');
-//         return;
-//     }
+form.addEventListener("submit", function(e){
+    e.preventDefault();
 
-//     function calculate(){
-//         if (age.value == '' || height.value == '' || weight.value == ''){
-//             alert('All input fields are required');
-//             return;
-//         } else {
-//             bmi();
-//         }
-//     }
+    const gender = document.querySelector("#gender").value;
+    const age = parseInt(document.querySelector("#age").value);
+    const height = parseInt(document.querySelector("#height").value);
+    const weight = parseInt(document.querySelector("#weight").value);
 
-//     //Calculate BMI
-//     const bmi = weight / (height * height);
-//     let bmiCategory = '';
+    if(!age || age <= 0 || isNaN(age)){
+        alert('Please enter a valid age.');
+        return;
+    } else if (!height || height <= 0 || isNaN(height)){
+        alert('Please enter a valid height.');
+        return;
+    } else if (!weight || weight <= 0 || isNaN(weight)){
+        alert('Please enter a valid weight.');
+        return;
+    }
 
-//     //Categorize BMI
-//     if (bmi < 18.5){
-//         bmiCategory = 'Underweight';
-//     } else if (bmi >= 18.5 && bmi < 24.9) {
-//         bmiCategory = 'Normal weight';
-//     }else if (bmi >= 25 && bmi < 29.9) {
-//         bmiCategory = 'Overweight';
-//     } else {
-//         bmiCategory = 'Obese'
-//     }
+    //Calculate BMI
+    const bmi = (weight / (height ** 2)).toFixed(2);
 
-//     //Display result
-//     const resultDiv = document.getElementById('result');
+    let bmiCategory = "";
+    if (bmi < 18.5){
+        bmiCategory = 'Underweight';
+     } else if (bmi >= 18.5 && bmi < 24.9) {
+         bmiCategory = 'Normal weight';
+    }else if (bmi >= 25 && bmi < 29.9) {
+         bmiCategory = 'Overweight';
+    } else {
+         bmiCategory = 'Obese'
+    }
+
+    //Display result
+    const result = document.getElementById('result');
+    result.innerHTML = `
+    <br>
+    <p><strong>Your BMI is:</strong> ${bmi}</p>
+    <p><strong>Category:</strong> ${bmiCategory}</p>
+    <br>
+    `;
+})
+
+
+
+
+// else {
+//     const bmi = (weight / ((height / 100) **2)).toFixed(2);
 //     resultDiv.innerHTML = `
 //     <br>
-//     <p><strong>Your BMI is:</strong> ${bmi.toFixed(2)}</p>
-//     <p><strong>Category:</strong> ${bmiCategory}</p>
-//     <br>
-//     `;
+// <p><strong>Your BMI is:</strong> ${bmi.toFixed(2)}</p>
+// <p><strong>Category:</strong> ${bmiCategory}</p>
+// <br>`
 // }
-
-
-
-// const form = document.querySelector("form");
-// const results = document.querySelector("#result");
-
-
-// form.addEventListener("submit", function(e){
-//     e.preventDefault();
-
-//     const gender = document.querySelector("gender").value;
-//     const age = parseInt(document.querySelector("age").value);
-//     const height = parseInt(document.querySelector("height").value);
-//     const weight = parseInt(document.querySelector("weight").value);
-
-//     if(!age || age <= 0 || isNaN(age)){
-//         alert('Please enter a valid age.');
-//         return;
-//     } else if (!height || height <= 0 || isNaN(height)){
-//         alert('Please enter a valid height.');
-//         return;
-//     } else if (!weight || weight <= 0 || isNaN(weight)){
-//         alert('Please enter a valid weight.');
-//         return;
-//     }else {
-        
-//     }
-// })
